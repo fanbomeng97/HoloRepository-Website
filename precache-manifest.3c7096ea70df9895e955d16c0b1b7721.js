e3490fe4b575ce0a993c883c7508f14291fb3f45
Fanbo Meng <46536567+fanbomeng97@users.noreply.github.com>
2019-08-18 17:54:48 +0100

    Updates

--- precache-manifest.3c7096ea70df9895e955d16c0b1b7721.js
+++ precache-manifest.3c7096ea70df9895e955d16c0b1b7721.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1a8be18c9b0a11fa7c4467b8117c862",
+    "revision": "d2214e3e75b0493fb7fa1f75e26887ce",
     "url": "/HoloRepository-Website/index.html"
   },
   {
-    "revision": "4fba754d246499067e2d",
-    "url": "/HoloRepository-Website/static/css/main.0f7377fe.chunk.css"
+    "revision": "38b99c14005a4887c5ab",
+    "url": "/HoloRepository-Website/static/css/main.abfeff29.chunk.css"
   },
   {
-    "revision": "3742602286e79c3e7cc8",
-    "url": "/HoloRepository-Website/static/js/2.573bdb99.chunk.js"
+    "revision": "5ae23d1754cce68e79b9",
+    "url": "/HoloRepository-Website/static/js/2.856cc779.chunk.js"
   },
   {
-    "revision": "4fba754d246499067e2d",
-    "url": "/HoloRepository-Website/static/js/main.ffa90404.chunk.js"
+    "revision": "38b99c14005a4887c5ab",
+    "url": "/HoloRepository-Website/static/js/main.035431d6.chunk.js"
   },
   {
     "revision": "3f8862f88a6e8213a1de",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HoloRepository-Website/precache-manifest.ed3d3449ee3aacd253f07d189d2ca23b.js"
+  "/HoloRepository-Website/precache-manifest.3c7096ea70df9895e955d16c0b1b7721.js"
 );
 
 self.addEventListener('message', (event) => {
