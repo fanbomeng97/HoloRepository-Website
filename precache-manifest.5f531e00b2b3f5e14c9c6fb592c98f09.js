12d59b2d0dd6fca7a209050f5dbba6ff16f1fdff
Fanbo Meng <46536567+fanbomeng97@users.noreply.github.com>
2019-08-18 01:42:19 +0100

    Updates

---
+++ precache-manifest.5f531e00b2b3f5e14c9c6fb592c98f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad626ad298e990817787cb3e3f9e8939",
+    "url": "/Introduction-Website/index.html"
+  },
+  {
+    "revision": "170536a3dc5e82141313",
+    "url": "/Introduction-Website/static/css/main.0f7377fe.chunk.css"
+  },
+  {
+    "revision": "cd6749672008fc7c9a38",
+    "url": "/Introduction-Website/static/js/2.fbdb54c8.chunk.js"
+  },
+  {
+    "revision": "170536a3dc5e82141313",
+    "url": "/Introduction-Website/static/js/main.c57b45aa.chunk.js"
+  },
+  {
+    "revision": "10491fc555f7662bbd7c",
+    "url": "/Introduction-Website/static/js/runtime~main.0b595081.js"
+  },
+  {
+    "revision": "451c266ff2130c8a2e14c147f48a9eaf",
+    "url": "/Introduction-Website/static/media/background.451c266f.jpg"
+  }
+]);
