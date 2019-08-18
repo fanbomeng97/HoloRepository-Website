ff9674a083e496e86ad1631a29289f842e546037
Fanbo Meng <46536567+fanbomeng97@users.noreply.github.com>
2019-08-18 19:35:16 +0100

    WEB: Add import for intro page

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SharedHeader from "./shared/Header";
 import SharedFooter from "./shared/Footer";
+import "./FontStyle.css";
 import {
     Header,
     Segment,
